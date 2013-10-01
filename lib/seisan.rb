9b06ff1f2d4f522baf685d83b3a99854707e18ac
SHIMADA Koji <koji.shimada@enishi-tech.com>
2013-10-01 14:30:24 +0900

    Move #report to Seisan::Task

--- lib/seisan.rb
+++ lib/seisan.rb
@@ -2,21 +2,6 @@ require 'seisan/version'
 require 'seisan/reporter'
 require 'seisan/base_renderer'
 require 'seisan/expense_renderer'
-require 'gimlet'
 
 module Seisan
-  def self.default_dest_base_path
-    File.join(Dir.pwd, 'output')
-  end
-
-  def self.report(config, dest_base_path=default_dest_base_path)
-    src_path = File.join(Dir.pwd, 'data', config[:target])
-    source = Gimlet::DataStore.new(src_path)
-
-    requests = source.to_h.values
-    exporter = Seisan::Reporter.new(requests, config)
-
-    dest_path = File.join(dest_base_path, '%s.xlsx' % config[:target].gsub('/', '-'))
-    exporter.export(dest_path)
-  end
 end
--- lib/seisan/task.rb
+++ lib/seisan/task.rb
@@ -1,5 +1,6 @@
 require 'rake'
 require 'seisan'
+require 'gimlet'
 
 module Seisan
   class Task
@@ -11,17 +12,33 @@ module Seisan
     def install
       desc "Generate seisan report"
       task :report do
-        report(ENV['target'])
+        config = {
+          organization_name: '株式会社えにしテック',
+          src_base_path: default_src_base_path,
+          dest_base_path: default_dest_base_path,
+          target: ENV['target'],
+        }
+        report(config)
       end
       task :default => :report
     end
 
-    def report(target)
-      config = {
-        organization_name: '株式会社えにしテック',
-        target: target,
-      }
-      Seisan.report(config)
+    def report(config)
+      source = Gimlet::DataStore.new(File.join(config[:src_base_path], config[:target]))
+
+      requests = source.to_h.values
+      exporter = Seisan::Reporter.new(requests, config)
+
+      dest_path = File.join(config[:dest_base_path], '%s.xlsx' % config[:target].gsub('/', '-'))
+      exporter.export(dest_path)
+    end
+
+    def default_src_base_path
+      File.join(Dir.pwd, 'data')
+    end
+
+    def default_dest_base_path
+      File.join(Dir.pwd, 'output')
     end
   end
 end
