6e46a72b069d0734caa6c0f8aa831ec82615a267
Yoji Shidara <dara@shidara.net>
2015-11-11 13:15:45 +0900

    Fail first

--- main.go
+++ main.go
@@ -19,15 +19,16 @@ func main() {
 	sr := reporter.New(*expense.NewReporter())
 
 	app.Action = func(c *cli.Context) {
-		if args := c.Args(); args.Present() {
-			target := args.First()
-
-			fmt.Printf("Processing %s ...\n", target)
-			if err := sr.Report(".", target); err != nil {
-				log.Fatal(err)
-			}
-		} else {
+		args := c.Args()
+		if !args.Present() {
 			fmt.Println("You must specify the 'TARGET'.\nExample:\n  % seisan 2015/10")
+			return
+		}
+
+		target := args.First()
+		fmt.Printf("Processing %s ...\n", target)
+		if err := sr.Report(".", target); err != nil {
+			log.Fatal(err)
 		}
 	}
 	app.Run(os.Args)
